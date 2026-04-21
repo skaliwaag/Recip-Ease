77b24f396f3c9bc3cefe8d8905b20bda2fe7fc50
Terysa <tandara@etsu.edu>
2026-04-21 00:40:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from dotenv import load_dotenv  
 # ***** need to make ".env" file for password stuff for our database, and make sure to hide it on windows*****
-
 import os 
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
@@ -13,11 +12,11 @@ password = os.getenv("MONGO_PASSWORD")
 
 # add connection string from MongoDB
 # we will need the db_passord from the .env file to complete this connection string
-uri = "mongodb+srv://roneys_db_user:<db_password>@recipe-ease.ajuvsog.mongodb.net/?appName=Recipe-Ease"
+uri = "mongodb+srv://<db_username>:<db_password>@recipe-ease.ajuvsog.mongodb.net/?appName=Recipe-Ease"
 
 # create connection to client
 # This will work once we add the cnnection string from mongoDB...
-client = MongoClient(uri, server_api=ServerApi{1})
+client = MongoClient(uri, server_api=ServerApi('1'))
 
 # ping the server to check the connection
 client.admin.command ('ping')
